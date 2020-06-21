4d81c03b6de2198f332863585b074f104103134e
sarahsyazwina <48936987+sarahsyazwina@users.noreply.github.com>
2020-06-21 21:48:03 +0800

    clean up

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,4 +1,5 @@
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -20,42 +21,43 @@ public class Main {
         
         //get the thrads, number of points, and time
         //points must be more than 1
-        while(n <= 1){
-            System.out.println("How many points?");
-            //n = k.nextInt();
-            n = 11;
-        }
+        do{
+            try{
+               System.out.println("How many points?");
+                n = k.nextInt();
+                //n = 11; 
+            }catch(InputMismatchException e){
+                k.nextLine();
+            }
+            
+        }while(n <= 1);
         //threads must be less than n
-        while(t <= 0 || n < t){
-            System.out.println("How many threads?");
-            //t = k.nextInt();
-            t = 3;
-        }
+        do{
+            try{
+               System.out.println("How many threads?");
+                t = k.nextInt();
+                //t = 3;
+            }catch(InputMismatchException e){
+                k.nextLine();
+            }
+            
+        }while(t <= 0 || n < t);
         //seconds must be more than 1
-        while(m <= 0){
-            System.out.println("How many seconds?");
-            //m = k.nextInt();
-            m = 90;
-        }
+        do{
+            try{
+               System.out.println("How many seconds?");
+                m = k.nextInt();
+                //m = 90;
+            }catch(InputMismatchException e){
+                k.nextLine();
+            }
+            
+        }while(m <= 0);
         
         //declare executor
         ExecutorService e = Executors.newFixedThreadPool(t);
         
         //declare game
         Game g = new Game(n, t, m, e);
-        
-//        //start threads
-//        ThreadController tc[] = new ThreadController[t];
-//        int count = 0;
-//        while(count < t){
-//            tc[count] = new ThreadController(g);
-//            e.submit(tc[count]);
-//            count++;
-//        }
-//        //set game start time now
-//        g.setStart();
-//        
-//        e.shutdownNow();
-//        
     }
 }
